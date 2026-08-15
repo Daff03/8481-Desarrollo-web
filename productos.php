832a3709e6036fde71211463b0ebcd3d60ce82ae
Daniel <danielferreira82005@gmail.com>
2026-08-15 03:23:16 -0400

    Agregar páginas base del sitio y estructura básica de index

---
+++ productos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Pagina Productos</title>
+
+  </head>
+
+  <body>
+    Pagina Productos<br>
+    <a href="index.php">Ir a Principal</a><br>
+    <a href="empresa.php">Ir a Empresa</a><br>
+    <a href="contacto.php">Ir a Contacto</a><br>
+    <a href="servicios.php">Ir a Servicios</a><br>
+    <a href="productos.php">Ir a Productos</a><br>
+    <a href="politica.php">Ir a Politica de Privacidad</a><br>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
