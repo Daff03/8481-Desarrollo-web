866b1d2115eaab1dc2e4c0f50592aca0619aa441
Daniel <danielferreira82005@gmail.com>
2026-09-14 23:10:47 -0300

    implementa footer con enlaces, secciones y estilos

---
+++ FrontEnd/components/footer.php
@@ -0,0 +1,36 @@
+<footer class="py-5" style="background-color: var(--color-primario); color: var(--color-texto-oscuro);">
+  <div class="container">
+    <div class="row g-4">
+
+      <div class="col-md-4">
+        <h5 style="color: var(--color-acento);">★ Al Sahara</h5>
+        <p class="footer-desc small">Cocina árabe auténtica en un ambiente familiar y acogedor.</p>
+      </div>
+
+      <div class="col-md-4">
+        <h6 style="color: var(--color-acento);">Enlaces</h6>
+        <ul class="list-unstyled small">
+          <li><a href="#" class="link-light text-decoration-none">Inicio</a></li>
+          <li><a href="#menu" class="link-light text-decoration-none">Menú</a></li>
+          <li><a href="#historia" class="link-light text-decoration-none">Nuestra Historia</a></li>
+          <li><a href="#" class="link-light text-decoration-none">Galería</a></li>
+          <li><a href="#contacto" class="link-light text-decoration-none">Contacto</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-4">
+        <h6 style="color: var(--color-acento);">Restaurante</h6>
+        <ul class="list-unstyled small">
+          <li>Ubicación</li>
+          <li>Términos de servicio</li>
+          <li>Política de privacidad</li>
+          <li>Email</li>
+        </ul>
+      </div>
+
+    </div>
+
+    <hr class="border-secondary my-4">
+    <p class="footer-desc small mb-0 text-center"> Al Sahara Restaurante. Todos los derechos reservados.</p>
+  </div>
+</footer>
